adea83739795cb7fa132984782e15910a2bcd414
Khai Jt <khai.jt@outlook.com>
2024-12-02 21:37:45 +0800

    Initialize services

---
+++ services/question_service.py
@@ -0,0 +1,47 @@
+from httpx import AsyncClient
+from models import question
+
+API_KEY = ""
+ENDPOINT = "https://assessment.takafulbrunei.com/v1/question"
+HEADERS = {"x-api-key": API_KEY}
+
+client = AsyncClient()
+
+async def answer_question_one(answer: question.Question1AnswerRequest) -> str:
+
+    response = await client.post(ENDPOINT + "/1", headers=HEADERS, json=answer)
+
+    if response.status_code == 422:
+        return "failed"
+    
+    # Validate the data of the response
+    data = response.json()
+
+    try:
+        question.Question1AnswerResponse.model_validate(data)
+    
+    except:
+        return "invalid"
+    
+    else:
+        return "success"
+
+
+async def answer_question_two(answer: question.Question2AnswerRequest) -> str:
+
+    response = await client.post(ENDPOINT + "/2", headers=HEADERS, json=answer)
+
+    if response.status_code == 422:
+        return "failed"
+    
+    # Validate the data of the response
+    data = response.json()
+
+    try:
+        question.Question2AnswerResponse.model_validate(data)
+    
+    except:
+        return "invalid"
+    
+    else:
+        return "success"
