3df62ef455f82902e47d050994ec590dc32aef52
Khai Jt <khai.jt@outlook.com>
2024-12-03 10:53:47 +0800

    Added new models

--- models/bank.py
+++ models/bank.py
@@ -1,6 +1,32 @@
 
 
-class Bank():
-    def __init__(self, name: str):
-        self.name = name
+
+
+class Customer:
+    def __init__(self, firstname: str, surname: str):
+        self.firstname = firstname
+        self.surname = surname
+
+
+class BankAccount:
+    def __init__(self, account_holder: Customer, balance: float):
+        self.account_holder = account_holder
+        self.balance = balance
+
+    def deposit(self, amount: int):
+        self.balance += amount
+        return self.balance
+
+    def withdraw(self, amount: int):
+        self.balance += amount
+        return self.balance
+
+
+    def get_balance(self, amount: int):
+        return self.balance
+
+
+
+
+    
 
